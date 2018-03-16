d9b44e400e1ef4f585bbc74464b545db6c89dd2f
stief <stefanbabukov98@gmail.com>
2018-03-16 17:03:46 +0200

    led stripe

--- project/led_strip/led_strip.ino
+++ project/led_strip/led_strip.ino
@@ -1,12 +1,41 @@
 #include <FastLED.h>
-#define LED_PIN     
-#define NUM_LEDS    
+#define LED_PIN     8
+#define NUM_LEDS    80
+int button = 7;
+int i = 0;
 CRGB leds[NUM_LEDS];
+
 void setup() {
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   
 }
-void loop() {
-  
 
+void loop() {
+  for(int i = 0; i < 80; i++) {
+    leds[i] = CRGB(0, 0, 0);
+    FastLED.show();
+    
+  }
+   for(int i = 0; i < 80; i++) {
+   while(digitalRead(button) == LOW); 
+    leds[i - 1] = CRGB(0, 0, 0);
+    leds[i] = CRGB(225, 0, 0);
+    //leds[i + 1] = CRGB(225, 0, 0);
+    FastLED.show();
+    //i++;
+    delay(50);
+    }
+    for(int i = 0; i < 80; i++) {
+    leds[i] = CRGB(0, 0, 0);
+    FastLED.show();
+    i++;
+  }    
+    
+        for(int i = 0; i < 80; i++) {
+        leds[i] = CRGB(0, 255, 0);
+        FastLED.show();
+        //i++;
+        }
+        while(digitalRead(button) == LOW); 
+        
 }
